90a18ff7f8f75e37bf6898922878dbd0adf84f82
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2025-03-02 18:34:45 -0500

    Agregar función para obtener leyenda por ID y mejorar documentación en servicios de leyendas

--- app/routes/legends_route.py
+++ app/routes/legends_route.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, HTTPException, status
 from app.core.db import SessionDep
-from app.services.legend_services import get_all_legends
+from app.services.legend_services import get_all_legends, get_legend_by_id
 from app.models.legend_model import Legend
 from app.schemas.legend_schema import LegendCreate
 
@@ -12,6 +12,15 @@ router = APIRouter(
 
 @router.get("", response_model=list[Legend])
 async def get_legends(session: SessionDep):
+    """
+    Obtiene todas las leyendas.
+
+    Args:
+        session (SessionDep): Sesión de la base de datos.
+
+    Returns:
+        list[Legend] | HTTPException: Lista de leyendas o un error 404.
+    """
     legends = get_all_legends(session)
 
     if legends is None:
@@ -21,3 +30,26 @@ async def get_legends(session: SessionDep):
         )
 
     return legends
+
+
+@router.get("/{legend_id}")
+async def get_legend(legend_id: int, session: SessionDep):
+    """
+    Obtiene una leyenda por su ID.
+
+    Args:
+        legend_id (int): ID de la leyenda.
+        session (SessionDep): Sesión de la base de datos.
+
+    Returns:
+        Legend | HTTPException: Leyenda o un error 404.
+    """
+    legend = get_legend_by_id(legend_id, session)
+
+    if legend is None:
+        return HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Leyenda no encontrada",
+        )
+
+    return legend
--- app/services/legend_services.py
+++ app/services/legend_services.py
@@ -2,10 +2,39 @@ from app.core.db import SessionDep
 from sqlmodel import select
 from app.models.legend_model import Legend
 
-def get_all_legends(session: SessionDep) -> list[Legend] | None: 
+
+def get_all_legends(session: SessionDep) -> list[Legend] | None:
+    """
+    Obtiene todas las leyendas de la base de datos.
+
+    Args:
+        session (SessionDep): Sesión de la base de datos.
+
+    Returns:
+        list[Legend] | None: Lista de leyendas o None si ocurre un error.
+    """
     try:
         legends = session.exec(select(Legend)).all()
-        
+
         return legends
+    except Exception as e:
+        return None
+
+
+def get_legend_by_id(legend_id: int, session: SessionDep) -> Legend | None:
+    """
+    Obtiene una leyenda por su ID.
+
+    Args:
+        legend_id (int): ID de la leyenda.
+        session (SessionDep): Sesión de la base de datos.
+
+    Returns:
+        Legend | None: Leyenda o None si ocurre un error.
+    """
+    try:
+        legend = session.get(Legend, legend_id)
+
+        return legend
     except Exception as e:
         return None
