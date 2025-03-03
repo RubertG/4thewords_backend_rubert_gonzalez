d6d3f3f97001552acfe8820ac86f4829820bff40
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2025-03-02 19:03:19 -0500

    Agregar función para actualizar leyenda y endpoint correspondiente en la ruta de leyendas

--- app/routes/legends_route.py
+++ app/routes/legends_route.py
@@ -4,9 +4,10 @@ from app.services.legend_services import (
     get_all_legends,
     get_legend_by_id,
     create_legend,
+    update_legend,
 )
 from app.models.legend_model import Legend
-from app.schemas.legend_schema import LegendCreate
+from app.schemas.legend_schema import LegendCreate, LegendUpdate
 
 router = APIRouter(
     prefix="/leyendas",
@@ -36,7 +37,7 @@ async def get_legends(session: SessionDep):
     return legends
 
 
-@router.post("", response_model=Legend)
+@router.post("", response_model=Legend, status_code=status.HTTP_201_CREATED)
 async def add_legend(legend_data: LegendCreate, session: SessionDep):
     """
     Crea una leyenda.
@@ -77,6 +78,29 @@ async def get_legend(legend_id: int, session: SessionDep):
         return HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail="Leyenda no encontrada",
+        )
+
+    return legend
+
+
+@router.patch("/{legend_id}", response_model=Legend, status_code=status.HTTP_200_OK)
+async def edit_legend(legend_id: int, legend_data: LegendUpdate, session: SessionDep):
+    """
+    Actualiza una leyenda por su ID.
+
+    Args:
+        legend_id (int): ID de la leyenda.
+        session (SessionDep): Sesión de la base de datos.
+
+    Returns:
+        Legend | HTTPException: Leyenda actualizada o un error 404 si no se encuentra.
+    """
+    legend = update_legend(legend_id, legend_data, session)
+
+    if legend is None:
+        return HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Error al actualizar la leyenda, no se encontró la leyenda",
         )
 
     return legend
--- app/services/legend_services.py
+++ app/services/legend_services.py
@@ -1,6 +1,7 @@
 from app.core.db import SessionDep
 from sqlmodel import select
 from app.models.legend_model import Legend
+from app.schemas.legend_schema import LegendCreate, LegendUpdate
 
 
 def get_all_legends(session: SessionDep) -> list[Legend] | None:
@@ -21,7 +22,7 @@ def get_all_legends(session: SessionDep) -> list[Legend] | None:
         return None
 
 
-def create_legend(legend_data: Legend, session: SessionDep) -> Legend | None:
+def create_legend(legend_data: LegendCreate, session: SessionDep) -> Legend | None:
     """
     Crea una leyenda en la base de datos. Primero valida los datos de la leyenda y luego si guarda en la base de datos.
 
@@ -59,5 +60,36 @@ def get_legend_by_id(legend_id: int, session: SessionDep) -> Legend | None:
         legend = session.get(Legend, legend_id)
 
         return legend
+    except Exception as e:
+        return None
+
+
+def update_legend(
+    legend_id: int, legend_data: LegendUpdate, session: SessionDep
+) -> Legend | None:
+    """
+    Actualiza una leyenda en la base de datos despues de verificar que exista.
+
+    Args:
+        legend_id (int): ID de la leyenda a actualizar.
+        legend_data (Legend): Datos de la leyenda.
+        session (SessionDep): Sesión de la base de datos.
+
+    Returns:
+        Legend | None: Leyenda actualizada o None si ocurre un error.
+    """
+    try:
+        legend_db = get_legend_by_id(legend_id, session)
+
+        if legend_db is None:
+            return None
+
+        legend_data_dict = legend_data.model_dump(exclude_unset=True)
+        legend_db.sqlmodel_update(legend_data_dict)
+        session.add(legend_db)
+        session.commit()
+        session.refresh(legend_db)
+
+        return legend_db
     except Exception as e:
         return None
