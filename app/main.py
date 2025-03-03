abbe052d065ebb6cb10e4e67779f4887f54aea32
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2025-03-02 23:13:21 -0500

    Agregar middleware CORS para permitir solicitudes desde cualquier origen

--- app/main.py
+++ app/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.core.db import create_all_tables
 from app.routes import legends_route
 
 app = FastAPI(lifespan=create_all_tables)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins="*",
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
